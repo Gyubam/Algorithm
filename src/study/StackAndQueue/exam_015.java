a5c72534b17b55312c8ae1cfd82e5c0b011e6d69
sgb8170@naver.com <sgb8170@naver.com>
2022-10-18 00:13:25 +0900

    수 정렬하기 1 (버블 정렬)

---
+++ src/study/StackAndQueue/exam_015.java
@@ -0,0 +1,42 @@
+package study.StackAndQueue;
+
+import java.util.*;
+
+//문제
+//N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+//
+//입력
+//첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수 주어진다. 이 수는 절댓값이 1,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
+//
+//출력
+//첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+
+public class exam_015 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] A = new int[N];
+		
+		for(int i=0;i<N;i++) {
+			A[i] = sc.nextInt();
+		}
+		
+		for(int i=0;i<N-1;i++) {
+			for(int j=0;j<N-1-i;j++) {
+				if(A[j] > A[j+1]) {
+					int temp = A[j];
+					A[j] = A[j+1];
+					A[j+1] = temp;
+				}
+			}
+		}
+		
+		for(int i=0;i<N;i++) {
+			System.out.println(A[i]);
+		}
+		
+	}
+
+}
